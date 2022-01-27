974165eb2b78692fcf4ed4f3661bba1edd677e69
Gogodu85 <47672649+Gogodu85@users.noreply.github.com>
2022-01-27 17:24:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function Calcul1(){
+							var a = document.getElementById('id1').value ;
+							var b = 4 
+							document.getElementById('id2').value =  parseInt(a) *  parseFloat(b);};
+							
+function Calcul2(){
+							var a = document.getElementById('id3').value ;
+							var b = 50 
+							document.getElementById('id4').value =  parseInt(a) *  parseFloat(b);};
+							
+function Calcul3(){
+							var a = document.getElementById('id5').value ;
+							var b = 7 
+							document.getElementById('id6').value =  parseInt(a) *  parseFloat(b);};
+							
+function Calcul4(){
+							var a = document.getElementById('id7').value ;
+							var b = 1.76 
+							document.getElementById('id8').value =  parseInt(a) *  parseFloat(b);};
+							
+function Calcul5(){
+							var a = document.getElementById('id9').value ;
+							document.getElementById('id10').value = (parseFloat(a)*(100/60)).toPrecision(3);};
